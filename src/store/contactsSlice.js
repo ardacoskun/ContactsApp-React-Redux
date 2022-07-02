4f431074b94fc832655507c52a4c0f724691b66d
Arda Coşkun <m.ardacoskun@gmail.com>
2022-07-02 13:56:42 +0300

    contactsAdapter created with createEntityAdapter in redux and used into contactsSlice

--- src/store/contactsSlice.js
+++ src/store/contactsSlice.js
@@ -1,9 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+
+export const contactsAdapter = createEntityAdapter();
+const initialState = contactsAdapter.getInitialState();
 
 export const contactsSlice = createSlice({
   name: "contacts",
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    addContact: contactsAdapter.addOne,
+  },
 });
 
+export const { addContact } = contactsSlice.actions;
 export default contactsSlice.reducer;
