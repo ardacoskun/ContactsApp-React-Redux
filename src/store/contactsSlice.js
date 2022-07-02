2cc4f7e03df68a188670bb99a858fddc9ea368f2
Arda Coşkun <m.ardacoskun@gmail.com>
2022-07-02 13:50:22 +0300

    redux contactsSlice created and imported into store

---
+++ src/store/contactsSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const contactsSlice = createSlice({
+  name: "contacts",
+  initialState: {},
+  reducers: {},
+});
+
+export default contactsSlice.reducer;
