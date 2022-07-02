a411a15bfbdad9f9cecb063173683eeaf1ef3c39
Arda Coşkun <m.ardacoskun@gmail.com>
2022-07-02 14:05:25 +0300

    contact name added to redux store with addContact dispatch in form component

--- src/components/Contacts/Form.js
+++ src/components/Contacts/Form.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addContact } from "../../store/contactsSlice";
+import { nanoid } from "@reduxjs/toolkit";
 
 const Form = () => {
   const [name, setName] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(addContact({ id: nanoid(), name }));
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={name}
